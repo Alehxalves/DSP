b505fd274b33628ef8f7b8fa65ed369f1b0689e3
alehxalves <alehxalves@gmail.com>
2022-09-04 17:26:16 -0300

    Lab 3

--- Lab 3/Desserializa.java
+++ Lab 3/Desserializa.java
@@ -1,14 +1,15 @@
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
+import java.util.Scanner;
 
 public class Desserializa {
 
-    public static void main(String[] args) {
+    public static void desserializarByPath(String path) {
         CarroEntity car = null;
-
         try {
-            FileInputStream fileIn = new FileInputStream("./carro.ser");
+            FileInputStream fileIn = new FileInputStream("./" + path);
             ObjectInputStream in = new ObjectInputStream(fileIn);
+
             car = (CarroEntity) in.readObject();
 
             fileIn.close();
@@ -17,5 +18,24 @@ public class Desserializa {
             e.printStackTrace();
         }
         System.out.println(car);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line;
+        while (true) {
+            System.out.println("<MENU>\n[1] Desserializar Arquivo. \n[2] Exit.");
+            line = scanner.nextLine();
+
+            if ("1".equals(line)) {
+                System.out.print("Digite o path do arquivo a ser Desserializado: ");
+                line = scanner.nextLine();
+                desserializarByPath(line);
+            } else if ("2".equals(line)) {
+                break;
+            } else
+                System.out.println("Opção inválida.");
+        }
+        scanner.close();
     }
 }
--- Lab 3/Serializa.java
+++ Lab 3/Serializa.java
@@ -1,7 +1,25 @@
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Serializa {
+
+    public static void serializar(List<CarroEntity> list) {
+        try {
+            for (CarroEntity carList : list) {
+                FileOutputStream fileOut = new FileOutputStream("./" + carList.getModelo().concat(".ser"));
+                ObjectOutputStream out = new ObjectOutputStream(fileOut);
+
+                out.writeObject(carList);
+                out.close();
+                fileOut.close();
+            }
+            System.out.println("Serializado com sucesso.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         CarroEntity car = new CarroEntity();
@@ -10,17 +28,16 @@ public class Serializa {
         car.setAno(2015);
         car.setFabricante("Lamborghini");
 
-        try {
-            FileOutputStream fileOut = new FileOutputStream("./carro.ser");
-            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+        CarroEntity car2 = new CarroEntity();
+        car2.setModelo("FERRARI F40");
+        car2.setCor("VERMELHO");
+        car2.setAno(1990);
+        car2.setFabricante("FERRARI");
 
-            out.writeObject(car);
-            out.close();
-            fileOut.close();
+        List<CarroEntity> carList = new ArrayList<>();
+        carList.add(car);
+        carList.add(car2);
 
-            System.out.println("Serializado com sucesso.");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        serializar(carList);
     }
 }
